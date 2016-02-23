b587807b79156f84d2db97d26fbdb25be2ff474c
Sebastian Proost <proost@W7DELL901002.mpimp-golm.mpg.de>
2016-02-23 14:54:52 +0100

    added legend

--- js/timelines.js
+++ js/timelines.js
@@ -70,16 +70,18 @@
 				
 				var step = (timeline_width-10)/(dataMax - dataMin);
 				
+				var legend_height = timeline_height+10;
+				
 				var svg = Pablo($( this )).svg({
 					width: timeline_width,
-					height: timeline_height
+					height: legend_height
 					});
 				
 				svg.line({  
 					x1:5,
-                    y1:timeline_height-3,
+                    y1:legend_height-3,
                     x2:timeline_width-5,
-                    y2:timeline_height-3,
+                    y2:legend_height-3,
                     stroke: '#BBB',
                     'stroke-width': 0.75})
 				
@@ -87,13 +89,28 @@
 				for (i = 1; i<(dataMax - dataMin); i++) {
 					svg.line({  
 						x1:5+step*i,
-						y1:timeline_height-8,
+						y1:legend_height-8,
 						x2:5+step*i,
-						y2:timeline_height-3,
+						y2:legend_height-3,
 						stroke: '#BBB',
 						'stroke-width': 0.75})					
 				}
 				// label
+				var labelMin = svg.text({
+					x:step-6, y: legend_height-18,
+					fill:'#BBB',
+					'font-size':'10px',
+					'font-family':'sans-serif'
+				});
+				labelMin.content((parseInt(dataMin)+1).toString());
 
+				var labelMax = svg.text({
+					x:step*((dataMax - dataMin)-1)-6, y: legend_height-18,
+					fill:'#BBB',
+					'font-size':'10px',
+					'font-family':'sans-serif'
+				});
+				labelMax.content((dataMax-1).toString());
+				
 			});
 		});
